dea461609d942cfb45df1202344637ac1d576e5d
d_train16 <diepganguly@Gmail.com>
2019-05-09 11:27:39 +1000

    work in progress to update phenotypes

--- upload_to_germinate/3_upload_phenotypes_iwyp60_germinate.R
+++ upload_to_germinate/3_upload_phenotypes_iwyp60_germinate.R
@@ -8,12 +8,26 @@ library(tidyverse)
 
 #### collate data
 iwyp_dir <- "iwyp60_data/"
-traits <- c("Harvest","Resp","Lidar","Biomass")
+traits <- c("Harvest", "Resp", "Lidar", "Biomass", "ASD", "Q2", "Physiology-Raw", "Physiology-BLUE")
 
 csv_fls <- dir(iwyp_dir, "csv") %>% tibble %>% 
   mutate(description = sapply(strsplit(., "_"), function(l) l[4])) %>%
   mutate(description = sapply(strsplit(description, ".csv"), function(l) l[1])) %>%
   filter(description %in% traits)
+
+keyfile <- read_delim(file.path(iwyp_dir, "Phenotypes_README.txt"), delim = '\t')
+
+dat <- mutate(csv_fls, datatype = sapply(strsplit(., "_"), function(l) l[4])) %>%
+  mutate(datatype = sapply(strsplit(datatype, ".csv"), function(l) l[1])) %>%
+  mutate(description = sapply(strsplit(., "_"), function(l) paste0(l[2],l[1],"_",l[3]))) %>%
+  mutate(site_name_short = sapply(strsplit(., "_"), function(l) l[2])) %>%
+  ## assume all GES sites are GES CR04 - need to make better !!!!!
+  mutate(site_name_short = sub(pattern = "GES", replacement = "GES CR04", x=site_name_short)) %>% 
+  mutate(experiment_id = tables$experiments$id[match(description,tables$experiments$description)]) %>% 
+  mutate(location_id = tables$locations$id[match(site_name_short, tables$locations$site_name_short)]) %>% 
+  select(experiment_id, location_id, description, datatype) %>%
+  ## andrew is global contract? probably needs edit ...
+  mutate(contact = 'andrew.bowerman@anu.edu.au')
 
 ## connect to database
 con <- dbConnect(MySQL(),
@@ -23,7 +37,7 @@ con <- dbConnect(MySQL(),
 
 ## get database tables
 table_names <- dbListTables(con)
-rq_tables <- c("phenotypes","experiments")
+rq_tables <- c("germinatebase","phenotypes","experiments","experimenttypes","units")
 tables <- lapply(FUN=dbReadTable, X=rq_tables, conn=con)
 ## give tables names to make calling specific table easier
 names(tables) <- rq_tables
